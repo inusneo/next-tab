2c6592d36aa2f0a974b8bd71f6ff8f55af37cc6d
inusneo <inusneo@gmail.com>
2025-05-13 15:25:29 +0900

    fix: 윈도우 배포 테스트

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,12 @@ import type { NextConfig } from "next";
 
 const isProd = process.env.NODE_ENV === 'production';
 
+const repoName = 'https://inusneo.github.io/next-tab/';
+
 const nextConfig: NextConfig = {
-  assetPrefix: isProd ? 'https://inusneo.github.io/next-tab' : '',
-  output: 'export',
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
+  trailingSlash: true,
   images: {
     unoptimized: true,
   }
--- scripts/createNoJekyll.ts
+++
@@ -1,7 +0,0 @@
-import fs from 'fs';
-import path from 'path';
-
-const filePath = path.join(__dirname, '..', 'out', '.nojekyll');
-
-fs.writeFileSync(filePath, '');
-console.log('.nojekyll 파일이 생성되었습니다.');
